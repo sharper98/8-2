42f9c354cc8478ac8536bf9fc8f03ec4be847636
Sean H <sharper1006@hotmail.com>
2023-02-15 11:08:24 -0500

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
+
+    const [card, setCard] = useState('');
+    const [isValid, setIsValid] = useState(null)
+
+  
+    
+    useEffect(() => {
+        console.log(card);
+        if (card !== ''){
+            setIsValid(luhnValidate(luhnCalculate(card)))
+        } 
+    }, [card])
+
+    const luhnCheckSum = (code) => {
+        let length = code.length;
+        let equalityCheck = length % 2;
+        /* console.log(length); */
+        let sum = 0;
+        for (let i = length-1; i >= 0; i--){
+            let d = parseInt(code.charAt(i))
+            /* console.log(d); */
+            if (i % 2 === equalityCheck){
+                d *= 2
+            }
+            if (d > 9) {
+                d -= 9
+            }
+            sum += d
+        }
+        return sum % 10
+    }
+
+    const luhnCalculate = (partcode) => {
+        let checkSum = luhnCheckSum(partcode + '0')
+        return checkSum === 0 ? 0 : 10 - checkSum;
+    }
+
+    const luhnValidate = (fullcode) => {
+        if ((fullcode % 2) === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }   
+
+    
+
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form>
+        <label for='input'>Enter Credit Card info:</label>
+        <input type='number' value={card} onChange={e => setCard(() => e.target.value)}></input>
+        </form>
+
+        {isValid === true? <p>card is valid</p>: null }
+        {isValid === false? <p>card isnt valid</p>: null }
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
